526b364602b94482158f40b07226b61866a80b9c
Cassie E Nicol <cassieenicol@gmail.com>
2019-06-08 08:22:20 -0600

    Initial test of Boost Parser, more refactoring is needed to make the into a separate module.

---
+++ include/TfOptions.hpp
@@ -0,0 +1,21 @@
+//
+// Created by cenicol on 6/8/19.
+//
+
+#ifndef TRASH_FILE_TFOPTIONS_HPP
+#define TRASH_FILE_TFOPTIONS_HPP
+
+#include <string>
+
+class TfOptions {
+public:
+    bool help;
+    bool version;
+    std::string desc;
+
+    TfOptions();
+
+};
+
+
+#endif //TRASH_FILE_TFOPTIONS_HPP
