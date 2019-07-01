88fb649800d9884a827b3ef256d2c65833a06c4c
Cassie E Nicol <cassieenicol@gmail.com>
2019-07-01 16:02:31 -0600

    adding Boost::Log-ing trivial

--- src/TfUtils/src/BoostParser.cpp
+++ src/TfUtils/src/BoostParser.cpp
@@ -1,13 +1,31 @@
 //
 // Created by cenicol on 6/8/19.
 //
+//
+//    This file is part of tf.
+//
+//    tf is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    tf is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with tf.  If not, see <https://www.gnu.org/licenses/>.
+//
 
-#include "include/TfUtils/BoostParser.hpp"
+#define BOOST_LOG_DYN_LINK 1
+
+#include <include/TfUtils/BoostParser.hpp>
+
+#include <TfUtils/ShowType.hpp>
 
 #include <iostream>
 #include <vector>
-
-#include <TfUtils/ShowType.hpp>
 
 #include <boost/program_options.hpp>
 #include <boost/log/trivial.hpp>
